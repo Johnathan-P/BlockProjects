2fdb1d87344c4327ddbe6cd8e72cedcc4e0497f1
Johnathan Pedersen <jpedersen2005@gmail.com>
2024-09-24 18:26:18 -0700

    added current clock folders

---
+++ block07/script.js
@@ -0,0 +1,7 @@
+const user = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+
+const x = 2+8;
+const y = 20*2;
+const z = 117/3;
+
+console.log("something funny");
