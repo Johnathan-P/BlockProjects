1a5c0fc9336df07dcd198f3def6f0011e37988e0
Johnathan Pedersen <jpedersen2005@gmail.com>
2024-10-20 08:28:31 -0700

    18 18

--- block18/script.js
+++ block18/script.js
@@ -12,7 +12,6 @@ function Multiply(a, b) {
     {
         console.error(error);
     }
-
 }
 //first error is if i pass a letter char
 function checkLetters(a, b)
@@ -30,4 +29,34 @@ function checkLetters(a, b)
     return +a * +b;
 }
 
+
+
+//second function to work on is concat stuff, 
+const c = [1, 25, 300, 2, 21, 16]
+const d = [176, 21, 22, 0, 45, 83476894576874]
+function concatOdds()
+{
+    //just merge the arrays
+    const merged = c.concat(d);
+    //num check
+    var res = merged.every(function(element) {return typeof element === 'number'});
+    //set!
+    if (res)
+    {
+        [...new Set(merged)];
+        //now remove all the even numbers
+        let oddNumbers = merged.filter(function(number) {
+            return number % 2 !== 0
+          }); 
+        //print our list
+        console.log(oddNumbers);
+    }
+    else
+    {
+            console.log("all elements must be a number");
+    }
+
+    
+}
+concatOdds();
 console.log(Multiply(a, b));
