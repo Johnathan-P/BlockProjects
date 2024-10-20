3c466affb2ce71350c5078d52c5d12d339d21bd8
Johnathan Pedersen <jpedersen2005@gmail.com>
2024-10-20 08:38:12 -0700

    19

---
+++ block19/script.js
@@ -0,0 +1,37 @@
+const freelancers = [
+    { name: "Dr. Slice", price: 25, occupation: "gardener" },
+    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
+    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
+    { name: "Prof. Prism", price: 81, occupation: "teacher" },
+    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
+    { name: "Prof. Spark", price: 76, occupation: "programmer" },
+    { name: "Dr. Wire", price: 47, occupation: "teacher" },
+    { name: "Prof. Goose", price: 72, occupation: "driver" },
+  ];
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+const populate = async () => {
+    await delay(5000);
+    let decide = getRandomInt(0, freelancers.length-1);
+    console.log(decide);
+        
+    let person = freelancers[decide];
+    let name = person.name;
+    let price = person.price;
+    let occupation = person.occupation;
+
+    const list = document.getElementById("lancers");
+            const newItem = document.createElement("li");
+            newItem.textContent = `NAME: ${name} PRICE: ${price} OCCUPATION: ${occupation}`
+            list.appendChild(newItem);
+    populate();
+}
+
+populate(freelancers);
