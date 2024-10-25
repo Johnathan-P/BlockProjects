0f4626b1c41193ac0999289efd8a977cf518867e
Johnathan Pedersen <jpedersen2005@gmail.com>
2024-10-24 18:01:05 -0700

    21

--- block21/script.js
+++ block21/script.js
@@ -1,14 +1,16 @@
-async function getEvents()
-{
-    const url = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/events`
-    const respone = await fetch(url);
-    const events = await respone.json();
-    console.log(events);
-    return events;
+async function getParties(){
+    const url = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2408-ftb-mt-web-pt/events'
+    const data = await fetch(url);
+    const json = await data.json();
+    return json;
 }
 
-async function renderEvents()
-{
-    let events = await getEvents();
-    console.log(getEvents());
+async function renderParties(){
+
 }
+
+async function main(){
+    const json = await getParties();
+    console.log(json);
+}
+main();
